6c64a537f5f579015483dd5634d4030c8669e76a
Sean Quinn <sean@jibsales.com>
2013-11-15 10:51:05 -0500

    Moved properties into req.git namespace to prevent clobbering other middleware

--- index.js
+++ index.js
@@ -14,22 +14,30 @@ module.exports = function (options) {
   if (!options.repos) throw new Error('You must include a valid directory as the "repos" option.');
   if (!fs.existsSync(options.repos)) throw new Error('Repos directory doesn\'t exist.');
 
+  function setupGitService (req) {
+    var match = req.url.match(router[req.method]);
+    return {
+      repo: path.join(options.repos, match[1]),
+      service: 'git-' + match[2] + '-pack'
+    }
+  }
+
   // -- Taken from https://github.com/substack/pushover
   function pack (s) {
     var n = (4 + s.length).toString(16);
     return Array(4 - n.length + 1).join('0') + n + s;
   }
 
-  function spawnGitService (req, res) {
+  function spawnGitService (req) {
 
     // -- Put together options to pass to git service
     var args = [];
     args.push('--stateless-rpc');
     if ('GET' === req.method) args.push('--advertise-refs');
-    args.push(req.repo);
+    args.push(req.git.repo);
     
     // -- Spawn the service and end the response on exit
-    return spawn('/usr/bin/' + req.service, args);
+    return spawn('/usr/bin/' + req.git.service, args);
   
   }
 
@@ -39,26 +47,23 @@ module.exports = function (options) {
     if ('GET' !== req.method && 'POST' !== req.method) return next();
     if (!router[req.method].test(req.url)) return next();
     
-    // -- Request setup
-    req.path = url.parse(req.url);
-    var match = req.url.match(router[req.method]);
-    req.repo = path.join(options.repos, match[1]);
-    req.service = 'git-' + match[2] + '-pack';
+    // -- Setup Git Service
+    req.git = setupGitService(req);
 
     // -- Set headers
     res.setHeader('Expires', 'Fri, 01 Jan 1980 00:00:00 GMT');
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate');
-    res.setHeader('Content-Type', 'application/x-' + req.service + '-advertisement');
+    res.setHeader('Content-Type', 'application/x-' + req.git.service + '-advertisement');
 
-    // -- Setup body of response to GET /:namespace/info/refs
+    // -- Write response body for GET /:namespace/info/refs
     if ('GET' === req.method) {
-      res.write(pack('# service=' + req.service + '\n'));
+      res.write(pack('# service=' + req.git.service + '\n'));
       res.write('0000');
     }
 
     // -- Spawn git service
-    var git = spawnGitService(req, res);
+    var git = spawnGitService(req);
     req.pipe(git.stdin);
     git.stdout.pipe(res);
     git.on('exit', function () {
